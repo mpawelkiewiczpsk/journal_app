7b4734dd820bca86092db32851dfb08caa428843
mpawelkiewiczpsk <mpawelkiewicz@tu.kielce.pl>
2024-03-18 12:50:42 +0100

    add routing and login screen

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import Root from "./routes/root";
+import ErrorPage from "./views/error-page.tsx";
+import Login from "./views/Login.tsx";
 import './index.css'
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root/>,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: "login",
+        element: <Login />,
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>,
 )
+
